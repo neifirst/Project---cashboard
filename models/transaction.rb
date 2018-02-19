917be0cad902ed6b1278c7bcb587050e0c884cd2
neifirst <e.mcculloch.arch@gmail.com>
2018-02-19 16:56:00 +0000

    Broken conversions

--- models/transaction.rb
+++ models/transaction.rb
@@ -1,5 +1,6 @@
 require('pry-byebug')
 require_relative( '../db/sql_runner' )
+require_relative( './money_convert' )
 
 class Transaction
 
@@ -10,16 +11,18 @@ class Transaction
     @id = options['id'].to_i if options['id']
     @date_time = options['date_time']
     @vendor = options['vendor']
-    @amount = options['amount']
+    binding.pry
+    @amount = MoneyConverter.convert_to_r(options['amount'].to_f)
     @tag_id = options['tag_id']
     @details = options['details']
   end
+
 
   def save()
     sql = "INSERT INTO transactions(vendor, amount, tag_id, details)
           VALUES($1, $2, $3, $4)
           RETURNING id"
-    values = [@vendor, ((100 * @amount.to_r).to_i), @tag_id, @details]
+    values = [@vendor, MoneyConverter.convert_to_i(@amount), @tag_id, @details]
     results = SqlRunner.run(sql, values)
     @id = results.first()['id'].to_i
   end
@@ -38,7 +41,7 @@ class Transaction
     sql = "UPDATE transactions
           SET(vendor, amount, tag_id, details) = ($1, $2, $3, $4)
           WHERE id = $5"
-    values = [@vendor, @amount, @tag_id, @details, @id]
+    values = [@vendor, MoneyConverter.convert_to_i(@amount), @tag_id, @details, @id]
     SqlRunner.run(sql, values)
   end
 
@@ -50,13 +53,6 @@ class Transaction
     SqlRunner.run(sql, values)
   end
 
-  def convert_to_rational
-    sql = "SELECT amount
-          FROM transactions
-          WHERE id = $1"
-    values = [@id]
-    return ((100 * SqlRunner.run(sql, values).to_r).to_i)
-
 
   def self.all()
     sql = "SELECT * FROM transactions"
@@ -66,10 +62,10 @@ class Transaction
 
 
   def self.total_amount()
-    sql = "SELECT amount FROM transactions"
-    results = (SqlRunner.run(sql)).map {|x| x.values}
-    pence = results.flatten.inject(0) {|sum, x| sum + x.to_i}
-    return '%.2f' % (pence.to_i/100.0)
+    sql = "SELECT SUM(amount) FROM transactions"
+    result = (SqlRunner.run(sql)).first
+    pence = result['sum']
+    return MoneyConverter.convert_to_r(pence)
   end
 
 
